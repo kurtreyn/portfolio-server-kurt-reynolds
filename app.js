007b19773927d0204d0e2decb2802b5dee665e2e
kurtreyn <kurtreyn@yahoo.com>
2022-09-08 14:32:06 -0400

    update CORS again

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
-  // res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header(
     'Access-Control-Allow-Headers',
