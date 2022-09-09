57ea1d669c9efaeed60e2693a7e1677e83612fbf
kurtreyn <kurtreyn@yahoo.com>
2022-09-09 00:34:24 -0400

    troubleshoot

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
   })
 );
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // res.header('Access-Control-Allow-Origin', 'https://kurtreyn.netlify.app/');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header(
     'Access-Control-Allow-Headers',
