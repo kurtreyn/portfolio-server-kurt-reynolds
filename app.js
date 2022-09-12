abf1e4277eb7e6dcd2c871471581f1f6ce954e49
kurtreyn <kurtreyn@yahoo.com>
2022-09-12 13:08:57 -0400

    CORS

--- app.js
+++ app.js
@@ -29,13 +29,13 @@ app.use(function (req, res, next) {
   // res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   // res.header('Access-Control-Allow-Origin', 'https://kurtreyn.netlify.app/');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers: Content-Type',
     'Access-Control-Allow-Origin',
     '*'
   );
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   // res.header(
   //   'Access-Control-Allow-Headers',
   //   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
