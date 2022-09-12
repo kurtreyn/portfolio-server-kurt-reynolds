f4ccc19b8013ac2a30306546e9f78fd23dfe4dea
kurtreyn <kurtreyn@yahoo.com>
2022-09-12 15:18:47 -0400

    CORS

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ connect.then(
 app.use(
   cors({
     // origin: 'http://localhost:3000',
-    // origin: 'https://kurtreyn.netlify.app/',
-    origin: '*',
+    origin: 'https://kurtreyn.netlify.app/',
+    // origin: '*',
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
   })
 );
@@ -30,11 +30,7 @@ app.use(function (req, res, next) {
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   // res.header('Access-Control-Allow-Origin', 'https://kurtreyn.netlify.app/');
   // res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers: Content-Type',
-    'Access-Control-Allow-Origin',
-    '*'
-  );
+  res.header('Access-Control-Allow-Origin', 'https://kurtreyn.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   // res.header(
   //   'Access-Control-Allow-Headers',
